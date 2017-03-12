5a3f167ee0eca332eb38555b7f6b962567d27293
Xien Dong <xiendong@outlook.com>
2017-03-12 16:22:12 +0800

    Add initial implementation for drag and drop

--- Jasmine/Grid/ViewControllers/GridGameViewController.swift
+++ Jasmine/Grid/ViewControllers/GridGameViewController.swift
@@ -1,13 +1,7 @@
-//
-//  GridGameViewController.swift
-//  Jasmine
-//
-//  Created by Xien Dong on 11/3/17.
-//  Copyright © 2017 nus.cs3217. All rights reserved.
-//
-
 import UIKit
 
+/// View Controller implementation for Grid Game.
+/// - Author: Wang Xien Dong
 class GridGameViewController: UIViewController {
 
     /* Constants */
@@ -23,14 +17,89 @@ class GridGameViewController: UIViewController {
     /* Properties */
     /// Stores a list of Chinese characters, which serves as the data source for  
     /// `charactersCollectionView`.
-    fileprivate var chineseChars: [Character]
-        = ["天", "翻", "地", "覆", "天", "翻", "地", "覆"]
+    fileprivate var chineseTexts: [String?]
+        = ["天", "翻", "地", "覆", nil, nil, nil, nil, "天", "翻", "地", "覆", nil, nil, nil, nil]
+
+    fileprivate var draggingTile: SquareTextViewCell?
+    fileprivate var draggingStartFrame: CGRect?
 
     /* View Controller Lifecycles */
     /// Readjusts layout (such as cell size) upon auto-rotate.
     override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
         charactersCollectionView.performBatchUpdates(charactersCollectionView.reloadData,
                                                      completion: nil)
+    }
+
+    /* Gesture Recognisers */
+    /// Listens to a drag gesture and handles the operation of dragging a tile, and dropping it
+    /// to another location.
+    @IBAction func onTilesDragged(_ sender: UIPanGestureRecognizer) {
+        let position = sender.location(in: charactersCollectionView)
+
+        switch sender.state {
+        case .began:
+            handleTileSelected(at: position)
+
+        case .changed:
+            handleTileDragged(at: position)
+
+        case .ended:
+            handleTileDropped(at: position)
+
+        default:
+            break
+        }
+    }
+}
+
+// MARK: - Drag and Drop Tiles
+fileprivate extension GridGameViewController {
+
+    fileprivate func handleTileSelected(at position: CGPoint) {
+        guard draggingTile == nil else {
+            return
+        }
+        guard draggingStartFrame == nil else {
+            return
+        }
+        guard let indexTouched = charactersCollectionView.indexPathForItem(at: position) else {
+            return
+        }
+        guard let cellTouched = charactersCollectionView
+            .cellForItem(at: indexTouched) as? SquareTextViewCell else {
+                return
+        }
+
+        draggingTile = cellTouched
+        draggingStartFrame = cellTouched.frame
+    }
+
+    fileprivate func handleTileDragged(at position: CGPoint) {
+        draggingTile?.frame.origin = position
+    }
+
+    fileprivate func handleTileDropped(at position: CGPoint) {
+        guard let draggingTile = draggingTile else {
+            return
+        }
+        guard let draggingStartFrame = draggingStartFrame else {
+            return
+        }
+        guard let indexLanded = charactersCollectionView.indexPathForItem(at: position) else {
+            return
+        }
+        guard let cellToVacate = charactersCollectionView
+            .cellForItem(at: indexLanded) as? SquareTextViewCell else {
+                return
+        }
+
+        UIView.animate(withDuration: 1.0, animations: {
+            draggingTile.frame = cellToVacate.frame
+            cellToVacate.frame = draggingStartFrame
+        }, completion: { _ in
+            self.draggingTile = nil
+            self.draggingStartFrame = nil
+        })
     }
 }
 
@@ -40,7 +109,7 @@ extension GridGameViewController: UICollectionViewDataSource {
     /// Tells the charactersCollectionView the number of cells to display.
     func collectionView(_ collectionView: UICollectionView,
                         numberOfItemsInSection section: Int) -> Int {
-        return chineseChars.count
+        return chineseTexts.count
     }
 
     /// Feeds the data (chinese characters) to the charactersCollectionView.
@@ -50,11 +119,11 @@ extension GridGameViewController: UICollectionViewDataSource {
         let reusableCell = collectionView.dequeueReusableCell(
             withReuseIdentifier: GridGameViewController.characterCellIdentifier, for: indexPath)
 
-        guard let characterCell = reusableCell as? GridCharacterViewCell else {
-            fatalError("View Cell that extends from GridChrarcterViewCell is required.")
+        guard let textCell = reusableCell as? SquareTextViewCell else {
+            fatalError("View Cell that extends from ChineseCharacterViewCell is required.")
         }
-        characterCell.chineseCharacter = chineseChars[indexPath.item]
-        return characterCell
+        textCell.text = chineseTexts[indexPath.item]
+        return textCell
     }
 }
 
