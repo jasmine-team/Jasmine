d3b3cf9271382bfcbd55e2b81e79573881b37d36
Xien Dong <xiendong@outlook.com>
2017-03-14 17:47:34 +0800

    implemented a base protocol for game VM and base delegate for game VC

---
+++ Jasmine/Common/ViewModels/BaseGameEngineProtocol.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+/// Sets the shared functionalities that are applicable across all game engine declarations.
+/// This protocol will be inherited by all the specialsied game engine view model protocols.
+///
+/// - Author: Wang Xien Dong
+protocol BaseGameEngineProtocol {
+
+}
