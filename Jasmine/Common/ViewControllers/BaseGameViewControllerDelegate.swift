a84d19bcacd92e9b0e08bc12637d7893aab7fd35
Xien Dong <xiendong@outlook.com>
2017-03-15 01:35:55 +0800

    add redisplay score and time remaining

--- Jasmine/Common/ViewControllers/BaseGameViewControllerDelegate.swift
+++ Jasmine/Common/ViewControllers/BaseGameViewControllerDelegate.swift
@@ -6,4 +6,17 @@ import Foundation
 /// - Author: Wang Xien Dong
 protocol BaseGameViewControllerDelegate {
 
+    typealias Seconds = Double
+
+    /// Redisplay the score displayed on the view controller screen with a new score.
+    ///
+    /// - Parameter newScore: the new score to be redisplayed.
+    func redisplay(_ newScore: Int)
+
+    /// Redisplay the time remaining on the view controller against a total time.
+    ///
+    /// - Parameters:
+    ///   - timeRemaining: the remaining time left, in seconds.
+    ///   - totalTime: the total time from the start of the game, in seconds.
+    func redisplay(_ timeRemaining: Seconds, outOf totalTime: Seconds)
 }
