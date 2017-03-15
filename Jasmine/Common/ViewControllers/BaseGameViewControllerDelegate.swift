949ed3855acf8dd87b6f9d0ce803c97d25f9076a
Xien Dong <xiendong@outlook.com>
2017-03-15 11:56:51 +0800

    update function declarations for score and time

--- Jasmine/Common/ViewControllers/BaseGameViewControllerDelegate.swift
+++ Jasmine/Common/ViewControllers/BaseGameViewControllerDelegate.swift
@@ -4,17 +4,25 @@ import Foundation
 /// This protocol will be inherited by all the specialsied game view controller delegates.
 protocol BaseGameViewControllerDelegate {
 
-    /* Score and Time Update */
+    /* Score Update */
+    /// A constant that indicates the initial score that is started with the game.
+    var initialScore: Int { get }
+
     /// Redisplay the score displayed on the view controller screen with a new score.
     ///
     /// - Parameter newScore: the new score to be redisplayed.
     func redisplay(_ newScore: Int)
+
+    /* Time Update */
+    /// A constant that indicates the initial time remaining that is started with the game.
+    var initialRemainingTime: TimeInterval { get }
 
     /// Redisplay the time remaining on the view controller against a total time.
     ///
     /// - Parameters:
     ///   - timeRemaining: the remaining time left, in seconds.
     ///   - totalTime: the total time from the start of the game, in seconds.
+    ///     If no bonus time is added, should equate to `initialRemainingTime`.
     func redisplay(_ timeRemaining: TimeInterval, outOf totalTime: TimeInterval)
 
     /* Game Status */
@@ -24,6 +32,6 @@ protocol BaseGameViewControllerDelegate {
     /// Note also that if the user has won/lost, the score in the `redisplay(_ newScore)` will be
     /// taken as the end game score. So update it before calling end game.
     ///
-    /// - Parameter newStatus: <#newStatus description#>
+    /// - Parameter newStatus: the new status of the current game session.
     func notifyGameStatus(with newStatus: GameStatus)
 }
