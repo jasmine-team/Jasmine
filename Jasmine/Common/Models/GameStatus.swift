976ceae2382c14fbbcc085038474c508d51381e7
Xien Dong <xiendong@outlook.com>
2017-03-15 20:29:57 +0800

    Remove timeout case

--- Jasmine/Common/Models/GameStatus.swift
+++ Jasmine/Common/Models/GameStatus.swift
@@ -6,11 +6,9 @@ import Foundation
 /// - inProgress: the game is currently in progress.
 /// - endedWithWon: the game has ended and the user has won.
 /// - endedWithLost: the game has ended and the user has lost.
-/// - endedWithTimeOut: the game has ended and the user ran out of time.
 enum GameStatus {
     case notStarted
     case inProgress
     case endedWithWon
     case endedWithLost
-    case endedWithTimeOut
 }
