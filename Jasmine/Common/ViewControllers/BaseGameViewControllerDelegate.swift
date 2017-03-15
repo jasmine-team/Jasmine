1f6f5071dc43d062c77b479a12f35531a73ba2a1
Xien Dong <xiendong@outlook.com>
2017-03-15 11:51:51 +0800

    added a method to indicate game status in view delegate

--- Jasmine/Common/ViewControllers/BaseGameViewControllerDelegate.swift
+++ Jasmine/Common/ViewControllers/BaseGameViewControllerDelegate.swift
@@ -4,6 +4,7 @@ import Foundation
 /// This protocol will be inherited by all the specialsied game view controller delegates.
 protocol BaseGameViewControllerDelegate {
 
+    /* Score and Time Update */
     /// Redisplay the score displayed on the view controller screen with a new score.
     ///
     /// - Parameter newScore: the new score to be redisplayed.
@@ -15,4 +16,14 @@ protocol BaseGameViewControllerDelegate {
     ///   - timeRemaining: the remaining time left, in seconds.
     ///   - totalTime: the total time from the start of the game, in seconds.
     func redisplay(_ timeRemaining: TimeInterval, outOf totalTime: TimeInterval)
+
+    /* Game Status */
+    /// Notifies the view controller that the game state has changed.
+    /// 
+    /// This is also the place to indicate when the user has won/lost.
+    /// Note also that if the user has won/lost, the score in the `redisplay(_ newScore)` will be
+    /// taken as the end game score. So update it before calling end game.
+    ///
+    /// - Parameter newStatus: <#newStatus description#>
+    func notifyGameStatus(with newStatus: GameStatus)
 }
