4158739a1708caf1836faef528d2394c5043b5c7
Xien Dong <xiendong@outlook.com>
2017-03-17 01:56:08 +0800

    add more test to Coordinates

--- JasmineTests/Common/Models/CoordinateTests.swift
+++ JasmineTests/Common/Models/CoordinateTests.swift
@@ -9,9 +9,22 @@ class CoordinateTests: XCTestCase {
     func testCoordinateEquals() {
         XCTAssertEqual(Coordinate(row: 3, col: 5), Coordinate(row: 3, col: 5),
                        "Coordinates with same row and col should be equal")
+
         XCTAssertNotEqual(Coordinate(row: 3, col: 5), Coordinate(row: 2, col: 5),
                           "Coordinates with different row should not be equal")
         XCTAssertNotEqual(Coordinate(row: 3, col: 5), Coordinate(row: 3, col: 6),
                           "Coordinates with different col should not be equal")
+        XCTAssertNotEqual(Coordinate(row: 3, col: 5), Coordinate(row: 5, col: 3),
+                          "Coordinates with different col should not be equal")
+    }
+
+    func testCoordinateHashValue() {
+        // Note that not equal is not tested due to possible hash collisions.
+
+        XCTAssertEqual(Coordinate(row: 3, col: 5).hashValue, Coordinate(row: 3, col: 5).hashValue,
+                       "Coordinates with same row and col should be equal")
+
+        XCTAssertEqual(Coordinate(row: -3, col: 5).hashValue, Coordinate(row: -3, col: 5).hashValue,
+                       "Coordinates with same row and col should be equal")
     }
 }
