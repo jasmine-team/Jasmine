4a4dadb248ccb858cefb014de7c248a35313be82
Xien Dong <xiendong@outlook.com>
2017-03-18 23:09:11 +0800

    add an initialiser that init view cell with a text view inside.

--- Jasmine/Common/Views/SquareTextViewCell.swift
+++ Jasmine/Common/Views/SquareTextViewCell.swift
@@ -1,13 +1,10 @@
 import UIKit
 import ChameleonFramework
 
-/// Represents a cell in the Grid Game collections view controller.
-/// This view cell assumes that a UILabel is stored within the cell for displaying Chinese character.
-///
-/// - Author: Wang Xien Dong
+/// Represents a cell in the Square Grid view controller.
 class SquareTextViewCell: UICollectionViewCell {
 
-    /* Properties */
+    // MARK: Properties
     /// Stores the displayed character by displaying it in the label.
     var text: String? {
         set {
@@ -30,5 +27,39 @@ class SquareTextViewCell: UICollectionViewCell {
             fatalError("This view cell requires a square text view contained in it.")
         }
         return textView
+    }
+
+    // MARK: Initialisers
+    /// Initialises this view cell and insert a square text view inside.
+    convenience init(withText text: String, andFrame frame: CGRect) {
+        self.init(frame: frame)
+
+        let squareTextView = SquareTextView()
+        squareTextView.text = text
+        setConstraints(to: squareTextView)
+        addSubview(squareTextView)
+    }
+
+    /// Default initialiser for collection view cell.
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    /// Default initialiser for collection view cell.
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    // MARK: Layout Constraints Methods
+    /// Sets the specified view to constraint to the four corners of this view cell.
+    ///
+    /// - Parameter view: the view to be subjected to constraints.
+    private func setConstraints(to view: UIView) {
+        for attribute in [NSLayoutAttribute.top, .bottom, .leading, .trailing] {
+            let layoutConstraint = NSLayoutConstraint(
+                item: view, attribute: attribute, relatedBy: .equal, toItem: self,
+                attribute: attribute, multiplier: 0, constant: 0)
+            addConstraint(layoutConstraint)
+        }
     }
 }
