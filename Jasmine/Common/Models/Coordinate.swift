a8fa3d64805eb279aa8fcbdfa09d1782f8defb58
Xien Dong <xiendong@outlook.com>
2017-03-18 17:14:45 +0800

    Remove toIndex implementation

--- Jasmine/Common/Models/Coordinate.swift
+++ Jasmine/Common/Models/Coordinate.swift
@@ -21,35 +21,6 @@ struct Coordinate {
         self.row = row
         self.col = col
     }
-
-    /// Initialise a coordinate based on a flattened array-like index, starting from 0, and 
-    /// `maxColumnCount`.
-    ///
-    /// - Parameters:
-    ///   - index: an index >= 0 to be converted.
-    ///   - maxColumnCount: maximum number of items in a row. Must be positive.
-    init(index: Int, withNumCol maxColumnCount: Int) {
-        guard index >= 0, maxColumnCount >= 1 else {
-            assertionFailure("Index should be >= 0, maxColumnCount should be >= 1.")
-            self.init(index: 0, withNumCol: 1)
-            return
-        }
-        let row = index / maxColumnCount
-        let col = index % maxColumnCount
-        self.init(row: row, col: col)
-    }
-
-    /// Computes the flattened array-like index starting from index 0 based on the `maxColumnCount`.
-    ///
-    /// - Parameter maxColumnCount: the maximum number of items possible in a row.
-    /// - Returns: the index based on the `maxColumnCount`, starting from index 0.
-    func toIndex(withNumCol maxColumnCount: Int) -> Int {
-        guard maxColumnCount > col else {
-            assertionFailure("maxColumnCount should be > col.")
-            return 0
-        }
-        return row * maxColumnCount + col
-    }
 }
 
 extension Coordinate: Hashable {
