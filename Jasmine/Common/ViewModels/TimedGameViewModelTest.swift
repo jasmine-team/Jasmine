a45bcbe390cf81724982ed3e173638286bf790ff
Herbert Ilhan Tanujaya <h.i@u.nus.edu>
2017-03-18 19:45:15 +0800

    a little cleanup

--- Jasmine/Common/ViewModels/TimedGameViewModelTest.swift
+++ Jasmine/Common/ViewModels/TimedGameViewModelTest.swift
@@ -4,23 +4,11 @@ import XCTest
 class TimedGameViewModelTest: XCTestCase {
 
     private let totalTimeAllowed: TimeInterval = 10
-    private var gameViewModel: TimedGameViewModel! // initialized in setUp
 
-    override func setUp() {
-        super.setUp()
-
-        gameViewModel = TimedGameViewModel(totalTimeAllowed: totalTimeAllowed)
-    }
-
-    func testTotalTimeAllowed() {
-        XCTAssertEqual(gameViewModel.totalTimeAllowed, totalTimeAllowed, "Total time allowed is wrong")
-    }
-
-    func testTimeRemaining() {
-        XCTAssertEqual(gameViewModel.timeRemaining, totalTimeAllowed, "Time remaining is wrong")
-    }
-
-    func testTimeElapsed() {
-        XCTAssertEqual(gameViewModel.timeElapsed, 0, "Time elapsed is wrong")
+    func testInit() {
+        let gameViewModel = TimedGameViewModel(totalTimeAllowed: totalTimeAllowed)
+        XCTAssertEqual(gameViewModel.totalTimeAllowed, totalTimeAllowed, "Total time allowed in init is wrong")
+        XCTAssertEqual(gameViewModel.timeRemaining, totalTimeAllowed, "Time remaining in init is wrong")
+        XCTAssertEqual(gameViewModel.timeElapsed, 0, "Time elapsed in init is wrong")
     }
 }
--- Jasmine/Flappy/ViewModels/FlappyViewModel.swift
+++ Jasmine/Flappy/ViewModels/FlappyViewModel.swift
@@ -1,3 +1,3 @@
-class FlappyViewModel {
+class FlappyViewModel: GameViewModel {
 
 }
