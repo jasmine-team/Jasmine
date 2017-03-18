a3a7fbe8f230806aea95b59fb1225903065c22ae
Li Kai <falconets25601@gmail.com>
2017-03-19 00:25:30 +0800

    pre load phrases at start app

--- Jasmine/Common/AppDelegate.swift
+++ Jasmine/Common/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,7 +9,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // swiftlint:disable:previous vertical_parameter_alignment
-        Scrapper.fillDatabase()
+
+        if UIApplication.isFirstLaunch {
+            Prebundler.hydrateRealm() // add phrases
+        }
         return true
     }
 
@@ -16,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state.
         // This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) 
         // or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. 
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks.
         // Games should use this method to pause the game.
     }
 
