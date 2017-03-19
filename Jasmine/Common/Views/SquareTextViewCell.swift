57afefb87d1eba7a24ee5b999e81340471a9e3bc
Xien Dong <xiendong@outlook.com>
2017-03-19 13:28:22 +0800

    Update to square text view cell to automatically instantiate a text view

--- Jasmine/Common/Views/SquareTextViewCell.swift
+++ Jasmine/Common/Views/SquareTextViewCell.swift
@@ -1,65 +1,62 @@
 import UIKit
-import ChameleonFramework
 
 /// Represents a cell in the Square Grid view controller.
+///
+/// Note that this view cell will instantiate and inject a `SquareTextView` automatically if not done
+/// so.
 class SquareTextViewCell: UICollectionViewCell {
 
     // MARK: Properties
     /// Stores the displayed character by displaying it in the label.
     var text: String? {
         set {
-            textView.text = newValue
+            textView?.text = newValue
         }
         get {
-            return textView.text
+            return textView?.text
         }
     }
 
-    /// Gets the label that is kept inside this view cell.
-    ///
-    /// An error is thrown if such a label is not found - a sign of wrongly applying this class onto
-    /// a view cell.
-    private var textView: SquareTextView {
-        let possibleView = contentView.subviews
-            .flatMap { $0 as? SquareTextView }
-            .first
-        guard let textView = possibleView else {
-            fatalError("This view cell requires a square text view contained in it.")
+    /// Gets the label that is kept inside this view cell. If such a view is not found, returns nil.
+    var textView: SquareTextView? {
+        get {
+            return contentView.subviews
+                .flatMap { $0 as? SquareTextView }
+                .first
         }
-        return textView
+        set {
+            guard let newTextView = newValue,
+                contentView.subviews.isEmpty else {
+                    return
+            }
+            
+            contentView.addSubview(newTextView)
+            newTextView.frame = bounds
+            contentView.bringSubview(toFront: newTextView)
+        }
+    }
+
+    /// Automatically enforce that the text view fills the entire boundary of the cell.
+    override var bounds: CGRect {
+        didSet {
+            textView?.frame = bounds
+        }
     }
 
     // MARK: Initialisers
-    /// Initialises this view cell and insert a square text view inside.
-    convenience init(withText text: String, andFrame frame: CGRect) {
-        self.init(frame: frame)
-
-        let squareTextView = SquareTextView()
-        squareTextView.text = text
-        setConstraints(to: squareTextView)
-        addSubview(squareTextView)
-    }
-
     /// Default initialiser for collection view cell.
     override init(frame: CGRect) {
         super.init(frame: frame)
+        initHelper()
     }
 
     /// Default initialiser for collection view cell.
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        initHelper()
     }
 
-    // MARK: Layout Constraints Methods
-    /// Sets the specified view to constraint to the four corners of this view cell.
-    ///
-    /// - Parameter view: the view to be subjected to constraints.
-    private func setConstraints(to view: UIView) {
-        for attribute in [NSLayoutAttribute.top, .bottom, .leading, .trailing] {
-            let layoutConstraint = NSLayoutConstraint(
-                item: view, attribute: attribute, relatedBy: .equal, toItem: self,
-                attribute: attribute, multiplier: 0, constant: 0)
-            addConstraint(layoutConstraint)
-        }
+    private func initHelper() {
+        textView = SquareTextView()
     }
 }
