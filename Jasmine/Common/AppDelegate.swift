c3ffe7fc913c55c91522553226669a2df1fcee0d
Li Kai <falconets25601@gmail.com>
2017-03-19 10:32:31 +0800

    add functionality to preload data

--- Jasmine/Common/AppDelegate.swift
+++ Jasmine/Common/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,7 +8,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // swiftlint:disable:previous vertical_parameter_alignment
+
+        if UIApplication.isFirstLaunch {
+            Prebundler.hydrateRealm() // add phrases
+        }
         return true
     }
 
@@ -15,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state.
         // This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) 
         // or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. 
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks.
         // Games should use this method to pause the game.
     }
 
