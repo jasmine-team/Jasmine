52fea74586519ccb4d17f30c1df905c29be7bc9a
Li Kai <falconets25601@gmail.com>
2017-03-19 17:05:18 +0800

    use swift way of declaring type

--- Jasmine/Common/Helpers/UIApplication+isFirstLaunch.swift
+++ Jasmine/Common/Helpers/UIApplication+isFirstLaunch.swift
@@ -3,7 +3,7 @@ import UIKit
 extension UIApplication {
 
     /// Determines whether this is the first time the user launched the application
-    static let isFirstLaunch = { _ -> Bool in
+    static let isFirstLaunch: Bool = {
         let launchedBeforeKey = "launchedBefore"
         let launchedBefore = UserDefaults.standard.bool(forKey: launchedBeforeKey)
         if !launchedBefore {    // set it if not set yet
