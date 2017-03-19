f80cafd4088203c20b4ff2b1dd296cfd7c09e893
riwu <riwu0730@gmail.com>
2017-03-19 19:10:46 +0800

    revert GameEngine -> ViewModel change

--- Jasmine/Common/ViewModels/BaseGameEngineProtocol.swift
+++ Jasmine/Common/ViewModels/BaseGameEngineProtocol.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// Sets the shared functionalities that are applicable across all view model declarations.
 /// This protocol will be inherited by all the specialised view model protocols.
-protocol BaseGameViewModelProtocol {
+protocol BaseGameEngineProtocol {
 
     // MARK: Properties
     /// Specifies the current score of the game. If the game has not started, it will be the initial
--- Jasmine/Flappy/ViewModels/FlappyGameEngineProtocol.swift
+++ Jasmine/Flappy/ViewModels/FlappyGameEngineProtocol.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-protocol FlappyGameViewModelProtocol: BaseGameViewModelProtocol {
+protocol FlappyGameViewModelProtocol: BaseGameEngineProtocol {
 
     var delegate: FlappyGameViewControllerDelegate? { get set }
 
