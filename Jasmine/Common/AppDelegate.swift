6adce5d5fa6a5b91039d58c052b6418c732b8fbd
Li Kai <falconets25601@gmail.com>
2017-03-19 11:26:51 +0800

    remove realm import

--- Jasmine/Common/AppDelegate.swift
+++ Jasmine/Common/AppDelegate.swift
@@ -1,5 +1,4 @@
 import UIKit
-import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state.
         // This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) 
         // or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. 
         // Games should use this method to pause the game.
     }
 
