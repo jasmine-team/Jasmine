739598c1de5e4cbf85cb702ed0df7a71c2dc564c
Xien Dong <xiendong@outlook.com>
2017-03-20 17:55:42 +0800

    this view controller delegate should not be deleted

--- Jasmine/Flappy/ViewControllers/FlappyViewControllerDelegate.swift
+++ Jasmine/Flappy/ViewControllers/FlappyViewControllerDelegate.swift
@@ -3,5 +3,5 @@ import Foundation
 /// Implement this delegate for `FlappyViewModelProtocol` to call for updating and commanding the
 /// implementing game view controller.
 protocol FlappyViewControllerDelegate: BaseGameViewControllerDelegate {
-    //TODO: Delete when not needed.
+    
 }
