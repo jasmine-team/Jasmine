26f55f58bb0482a4fdbb2b105b786a341348bd06
Xien Dong <xiendong@outlook.com>
2017-03-20 17:41:49 +0800

    Added basic implementation to Flappy Delegate and Protocols

---
+++ Jasmine/Flappy/ViewControllers/FlappyViewControllerDelegate.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+/// Implement this delegate for `FlappyViewModelProtocol` to call for updating and commanding the
+/// implementing game view controller.
+protocol FlappyViewControllerDelegate: BaseGameViewControllerDelegate {
+    //TODO: Delete when not needed.
+}
