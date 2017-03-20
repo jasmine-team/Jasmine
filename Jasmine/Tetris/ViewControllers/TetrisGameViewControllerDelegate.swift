4fc6785783aca7b3675e39beaf34f17e60897886
Wang Xien Dong <xdrawks@users.noreply.github.com>
2017-03-20 17:25:32 +0800

    reorder functions

--- Jasmine/Tetris/ViewControllers/TetrisGameViewControllerDelegate.swift
+++ Jasmine/Tetris/ViewControllers/TetrisGameViewControllerDelegate.swift
@@ -5,14 +5,14 @@ protocol TetrisGameViewControllerDelegate: BaseGameViewControllerDelegate {
     /// Tells the view controller to retrieve `upcomingTiles` and reload the view for the upcoming tiles.
     func redisplayUpcomingTiles()
 
-    /// Shifts the content of the tiles from Coordinate `from` to Coordinate `to`
-    ///
-    /// - Parameter coordinatesShifted: array of coordinates to shift
-    func animate(shiftTiles coordinatesToShift: [(from: Coordinate, to: Coordinate)])
-
     // MARK: Animation
     /// Ask the view controller to animate the destruction of tiles at the specified coordinates.
     ///
     /// - Parameter coodinates: the set of coordinates to be destroyed.
     func animate(destroyTilesAt coodinates: Set<Coordinate>)
+    
+    /// Shifts the content of the tiles from Coordinate `from` to Coordinate `to`
+    ///
+    /// - Parameter coordinatesShifted: array of coordinates to shift
+    func animate(shiftTiles coordinatesToShift: [(from: Coordinate, to: Coordinate)])
 }
