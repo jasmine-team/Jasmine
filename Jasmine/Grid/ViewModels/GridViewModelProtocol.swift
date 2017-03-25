d96bb639aaa7ffe9cc842cbf7930ead42b589e2c
Herbert Ilhan Tanujaya <h.i@u.nus.edu>
2017-03-25 09:28:05 +0800

    add protocols + update comments in Grid protocols

--- Jasmine/Grid/ViewModels/GridViewModelProtocol.swift
+++ Jasmine/Grid/ViewModels/GridViewModelProtocol.swift
@@ -15,14 +15,14 @@ protocol GridViewModelProtocol: BaseViewModelProtocol, CountdownTimable {
     /// Tells the Game Engine View Model that the user from the View Controller attempts to swap
     /// the specified two tiles.
     ///
-    /// Note that if the tiles are swapped, `delegate.updateTiles(...)` should be called to update
+    /// Note that if the tiles are swapped, `delegate.updateGridData` should be called to update
     /// the grid data of the cell stored in the view controller. However, there is no need to call
-    /// `delegate.redisplayTiles(...)` as it will be done implicitly by the view controller when
+    /// `delegate.redisplayAllTiles` as it will be done implicitly by the view controller when
     /// the swapping is successful (determined by the returned value).
     ///
     /// - Parameters:
     ///   - coord1: One of the cells to be swapped.
     ///   - coord2: The other cell to be swapped.
-    /// - Returns: Returns true if the two coordinates has be swapped, false otherwise.
+    /// - Returns: Returns true if the two coordinates has swapped, false otherwise.
     func swapTiles(_ coord1: Coordinate, and coord2: Coordinate) -> Bool
 }
--- Jasmine/Sliding/ViewControllers/SlidingGameViewControllerDelegate.swift
+++ Jasmine/Sliding/ViewControllers/SlidingGameViewControllerDelegate.swift
@@ -1,15 +1,15 @@
 import Foundation
 
-/// Implement this delegate for `GridViewModelProtocol` to call for updating and commanding the
+/// Implement this delegate for `SlidingViewModelProtocol` to call for updating and commanding the
 /// implementing game view controller.
-protocol GridGameViewControllerDelegate: BaseGameViewControllerDelegate {
+protocol SlidingGameViewControllerDelegate: BaseGameViewControllerDelegate {
 
     /// Update the grid data stored in the Grid Game View Controller with a new dataset.
     func updateGridData()
 
     /// Refreshes the tiles based on the tiles information stored in the View Controller's grid data.
     ///
-    /// Note to call `update(tilesWith newGridData)` if any information in the grid data should be
+    /// Note to call `updateGridData` if any information in the grid data should be
     /// updated.
     func redisplayAllTiles()
 }
