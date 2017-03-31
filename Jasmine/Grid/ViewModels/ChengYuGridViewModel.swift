4dedd8b46de14a3c24ea33fcf7ed0cdcdd5f9a27
Herbert Ilhan Tanujaya <h.i@u.nus.edu>
2017-03-31 14:41:17 +0800

    add chengyu grid

--- Jasmine/Grid/ViewModels/ChengYuGridViewModel.swift
+++ Jasmine/Grid/ViewModels/ChengYuGridViewModel.swift
@@ -1,8 +1,15 @@
 import Foundation
 
 class ChengYuGridViewModel: BaseGridViewModel {
-    // init(time: TimeInterval, gameData: GameData) {
-    //     super.init(time: time, answers: phrases)
-    //     gameTitle = "Cheng Yu (成语) Grid Game"
-    // }
+    init(time: TimeInterval, gameData: GameData, numberOfPhrases: Int) {
+        let phrases = gameData.phrases.prefix(numberOfPhrases)
+
+        let answers = phrases.map { $0.chinese.characters.map { char in String(char) } }
+        let tiles = answers.flatMap { $0 }
+
+        super.init(time: time, tiles: tiles, possibleAnswers: answers,
+                   rows: numberOfPhrases, columns: Constants.Game.Grid.columns)
+
+        gameTitle = "Cheng Yu (成语) Grid Game"
+    }
 }
