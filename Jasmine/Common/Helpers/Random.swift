684aa452ff4834473c460b3e5cc1a1bf48590b09
riwu <riwu0730@gmail.com>
2017-04-01 18:51:42 +0800

    Added toExclusive to Random

--- Jasmine/Common/Helpers/Random.swift
+++ Jasmine/Common/Helpers/Random.swift
@@ -10,8 +10,19 @@ enum Random {
     ///   - toInclusive: end of range, inclusive
     /// - Returns: a random number in the range
     static func integer(from: Int = 0, toInclusive: Int) -> Int {
-        assert(toInclusive > from, "`toInclusive` must be larger than `from` number")
+        assert(toInclusive > from, "`toInclusive` must be larger than `from`")
         return from + Int(arc4random_uniform(UInt32(toInclusive - from + 1)))
+    }
+
+    /// Generates a random integer
+    ///
+    /// - Parameters:
+    ///   - from: start of range, inclusive
+    ///   - toExclusive: end of range, exclusive
+    /// - Returns: a random number in the range
+    static func integer(from: Int = 0, toExclusive: Int) -> Int {
+        assert(toExclusive > from + 1, "`toExclusive` must be larger than `from` by at least 2")
+        return from + Int(arc4random_uniform(UInt32(toExclusive - from)))
     }
 
     /// Generates a random double
