d26071b4d26fc0d0db49220d7fdb1380ab1164b6
Xien Dong <xiendong@outlook.com>
2017-04-03 21:13:51 +0800

    Sliding game conformance update

--- Jasmine/Sliding/ViewModels/MockSlidingViewModel.swift
+++ Jasmine/Sliding/ViewModels/MockSlidingViewModel.swift
@@ -10,20 +10,12 @@ class MockSlidingViewModel: SlidingViewModelProtocol, CountdownTimable {
     weak var delegate: SlidingGameViewControllerDelegate?
 
     /// Stores the grid data that will be used to display in the view controller.
-    var gridData: [Coordinate: String] = [
-        Coordinate(row: 0, col: 0): "A",
-        Coordinate(row: 0, col: 1): "B",
-        Coordinate(row: 0, col: 2): "C",
-        Coordinate(row: 0, col: 3): "D",
-        Coordinate(row: 1, col: 0): "E",
-        Coordinate(row: 1, col: 1): "F",
-        Coordinate(row: 1, col: 2): "G",
-        Coordinate(row: 1, col: 3): "H",
-        Coordinate(row: 2, col: 0): "I",
-        Coordinate(row: 2, col: 1): "J",
-        Coordinate(row: 2, col: 2): "K",
-        Coordinate(row: 2, col: 3): "K",
-    ]
+    var gridData: TextGrid = TextGrid(fromInitialGrid: [
+        ["A", "B", "C", "D"],
+        ["A", "B", "C", "D"],
+        ["A", "B", "C", "D"],
+        ["A", "B", "C", nil]
+    ])
 
     // MARK: Game Operations
     /// Tells the Game Engine View Model that the user from the View Controller attempts to slide
@@ -39,30 +31,13 @@ class MockSlidingViewModel: SlidingViewModelProtocol, CountdownTimable {
     ///   - end: The destination of the sliding tile. Should be empty.
     /// - Returns: Returns true if the tile successfully slided, false otherwise.
     func slideTile(from start: Coordinate, to end: Coordinate) -> Bool {
-        guard !gridData.keys.contains(end) else {
-            return false
-        }
-        gridData[end] = gridData.removeValue(forKey: start)
-        delegate!.updateGridData()
-        return true
+        // TODO: Will be implemented by actual VM.
+        return false
     }
 
     func canTileSlide(from coordinate: Coordinate) -> [Direction: Coordinate] {
-        var allowedDirections: [Direction: Coordinate] = [:]
-
-        if !gridData.keys.contains(coordinate.nextCol) {
-            allowedDirections[.eastwards] = coordinate.nextCol
-        }
-        if !gridData.keys.contains(coordinate.prevCol) {
-            allowedDirections[.westwards] = coordinate.prevCol
-        }
-        if !gridData.keys.contains(coordinate.nextRow) {
-            allowedDirections[.southwards] = coordinate.nextRow
-        }
-        if !gridData.keys.contains(coordinate.prevRow) {
-            allowedDirections[.northwards] = coordinate.prevRow
-        }
-        return allowedDirections
+        // TODO: Will be implemented by actual VM.
+        return [:]
     }
 }
 
