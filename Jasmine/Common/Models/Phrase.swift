5c58e3b1a7cdda646423a250bdd3124de52f62c5
Li Kai <falconets25601@gmail.com>
2017-04-06 11:47:21 +0800

    Implement new phrase model

--- Jasmine/Common/Models/Phrase.swift
+++ Jasmine/Common/Models/Phrase.swift
@@ -1,22 +1,35 @@
 import RealmSwift
 
-/// Phrase model to store strings of various languages, read-only, can contain more than one word
+/// Phrase model to store strings of various languages, read-only except for timeSeen
 class Phrase: Object {
-
-    /// Information should be read only during operation
-    dynamic fileprivate(set) var chinese: String = ""
-    dynamic fileprivate(set) var pinyin: String = ""
-    dynamic fileprivate(set) var english: String = ""
-
-    /// Meaning of the word in english
-    dynamic fileprivate(set) var englishMeaning: String = ""
-    dynamic fileprivate(set) var chineseMeaning: String = ""
 
     /// Rank of the frequency of word in practice, higher means more difficult
     // -1 for invalid rank, realm doesn't support init without defaults
-    dynamic fileprivate(set) var rank: Int = -1
+    private dynamic var rank: Int = -1
+
+    /// Information should be read only during operation
+    private dynamic var rawChinese: String = ""
+    private dynamic var rawPinyin: String = ""
+    private(set) dynamic var english: String = ""
+
+    /// Meaning of the word in english
+    private(set) dynamic var englishMeaning: String = ""
+    private(set) dynamic var chineseMeaning: String = ""
 
     override static func primaryKey() -> String? {
-        return "chinese"
+        return "rawChinese"
     }
+
+    /// MARK: ignoredProperties
+    var chinese: [String] {
+        return rawChinese.components(separatedBy: "")
+    }
+    var pinyin: [String] {
+        return rawPinyin.components(separatedBy: " ")
+    }
+
+    override static func ignoredProperties() -> [String] {
+        return ["chinese", "pinyin"]
+    }
+
 }
