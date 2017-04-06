56fa1ca437c19fadba2fe01e08f5de09438a54d1
Xien Dong <xiendong@outlook.com>
2017-04-06 22:06:03 +0800

    delete accidentally included files

--- Jasmine/Tetris/GameCommons/ViewControllers/Delegates/HighlightedUpdateDelegate.swift
+++
@@ -1,6 +0,0 @@
-protocol HighlightedUpdateDelegate: class {
-
-    /// Tells the implementor of the delegate that the highlighted coordinates have been changed.
-    /// - Note: This method can be used to update the highlighted coordinates that is displayed on views.
-    func highlightedCoordinatesDidUpdate()
-}
--- Jasmine/Tetris/GameCommons/ViewControllers/Delegates/TimeUpdateDelegate.swift
+++
@@ -1,6 +0,0 @@
-protocol TimeUpdateDelegate: class {
-
-    /// Tells the implementor of the delegate that the time has been updated.
-    /// - Note: This method can be used to update the remaining time that is displayed on views.
-    func timeDidUpdate()
-}
