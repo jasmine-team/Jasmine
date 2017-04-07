ac14ed2a3b36b2eff0f83fb017da92c22156f32f
Li Kai <li-kai@users.noreply.github.com>
2017-04-07 23:41:15 +0800

    Game center integration (#101)
    * Fix sound service
    
    * Add buttons to show game center
    
    * Remove print statements
    
    * Make the buttons reflect order in GameCenterViewController

---
+++ Jasmine/Profile/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,48 @@
+import UIKit
+import GameKit
+
+class ProfileViewController: UIViewController {
+
+    @IBAction func showAchievements(_ sender: UIButton) {
+        segueTo(.achievements)
+    }
+
+    @IBAction func showLeaderboards(_ sender: UIButton) {
+        segueTo(.leaderboards)
+    }
+
+}
+
+// Delegate to dismiss the GC controller
+extension ProfileViewController: GKGameCenterControllerDelegate {
+
+    func segueTo(_ viewState: GKGameCenterViewControllerState) {
+        let localPlayer = GKLocalPlayer.localPlayer()
+        if localPlayer.isAuthenticated {
+            helperPresent(viewState)
+        }
+        localPlayer.authenticateHandler = { viewController, error in
+            if let viewController = viewController {
+                // 1. Show login if player is not logged in
+                self.present(viewController, animated: true)
+            } else if localPlayer.isAuthenticated {
+                self.segueTo(viewState)
+            } else {
+                // Game center is disabled for user
+                return
+            }
+        }
+    }
+
+    func helperPresent(_ viewState: GKGameCenterViewControllerState) {
+        let gameCenterViewController = GKGameCenterViewController()
+        gameCenterViewController.gameCenterDelegate = self
+        gameCenterViewController.viewState = viewState
+        self.present(gameCenterViewController, animated: true, completion: nil)
+    }
+
+    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+        gameCenterViewController.dismiss(animated: true)
+    }
+
+}
