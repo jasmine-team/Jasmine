fe53e8359233fdf9b000eebc0c39a9fa3504486f
likai <likai@electabuzzs-imac-8.local>
2017-04-07 18:19:38 +0800

    Fix sound service

--- Jasmine/Common/Helpers/SoundService.swift
+++ Jasmine/Common/Helpers/SoundService.swift
@@ -15,10 +15,12 @@ class SoundService: NSObject {
     fileprivate var effectPlayers: [Effect: [AVAudioPlayer]] = [:]
 
     private override init() {
+        super.init()
         for background in Background.allValues {
             do {
                 let player = try AVAudioPlayer(filename: background.rawValue)
                 player.prepareToPlay()
+                player.delegate = self
                 backgroundPlayers[background] = player
             } catch {
                 assertionFailure("Could not instantiate background sound")
@@ -28,6 +30,7 @@ class SoundService: NSObject {
             effectPlayers[effect] = (0..<Effect.concurrentLimit).flatMap { _ in
                 let player = try? AVAudioPlayer(filename: effect.rawValue)
                 player?.prepareToPlay()
+                player?.delegate = self
                 return player
             }
         }
@@ -76,7 +79,7 @@ extension SoundService: AVAudioPlayerDelegate {
         DispatchQueue.global(qos: .background).async {
             player.prepareToPlay()
         }
-
+        print("DID FINISH PLAYING", player)
         // ignore if other background sound is playing
         guard isBackgroundPlaying == false else {
             assertionFailure("No two background sound should be playing at the same time")
@@ -85,6 +88,7 @@ extension SoundService: AVAudioPlayerDelegate {
         let nextSound = Background.defaultPlaylist.shuffled().first(where: { sound in
             return backgroundPlayers[sound] != player
         })
+        print("NEXT PLAYER", nextSound ?? "No available players")
         if let nextSound = nextSound {
             play(nextSound)
         }
