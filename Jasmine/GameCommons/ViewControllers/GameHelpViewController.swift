ad6ab5bbaac89ba5b795d7f69b1fe6d8b4cab0a0
Xien Dong <xiendong@outlook.com>
2017-04-08 00:16:10 +0800

    Adding game help vc

---
+++ Jasmine/GameCommons/ViewControllers/GameHelpViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GameHelpViewController.swift
+//  Jasmine
+//
+//  Created by Xien Dong on 7/4/17.
+//  Copyright © 2017 nus.cs3217. All rights reserved.
+//
+
+import UIKit
+
+class GameHelpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
