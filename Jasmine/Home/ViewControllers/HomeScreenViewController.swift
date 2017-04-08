b8817039d1f10b1a1518503df173ba405340ab4c
Li Kai <falconets25601@gmail.com>
2017-04-08 15:51:29 +0800

    Change naming of factory to manager

--- Jasmine/Home/ViewControllers/HomeScreenViewController.swift
+++ Jasmine/Home/ViewControllers/HomeScreenViewController.swift
@@ -7,36 +7,36 @@ class HomeScreenViewController: UIViewController {
     @IBOutlet private weak var slidingCiHuiButton: UIButton!
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let gameDataFactory = try? GameDataFactory() else {
+        guard let gameManager = try? GameManager() else {
             fatalError("Error with Realm")
         }
 
         let level = Level()
         if let swappingGame = segue.destination as? SwappingGameViewController {
             if (sender as? UIButton) === swappingChengYuButton {
-                let gameData = gameDataFactory.createGame(fromLevel: level)
+                let gameData = gameManager.createGame(fromLevel: level)
                 swappingGame.segueWith(ChengYuSwappingViewModel(time: GameConstants.Swapping.time,
                                                                 gameData: gameData,
                                                                 numberOfPhrases: GameConstants.Swapping.rows))
             } else if (sender as? UIButton) === swappingCiHuiButton {
                 level.gameType = .ciHui
-                let gameData = gameDataFactory.createGame(fromLevel: level)
+                let gameData = gameManager.createGame(fromLevel: level)
                 swappingGame.segueWith(CiHuiSwappingViewModel(time: GameConstants.Swapping.time,
                                                               gameData: gameData,
                                                               numberOfPhrases: GameConstants.Swapping.rows))
             }
         } else if let tetrisGame = segue.destination as? TetrisGameViewController {
-            let gameData = gameDataFactory.createGame(fromLevel: level)
+            let gameData = gameManager.createGame(fromLevel: level)
             tetrisGame.segueWith(TetrisGameViewModel(gameData: gameData))
         } else if let slidingGame = segue.destination as? SlidingGameViewController {
             if (sender as? UIButton) === slidingChengYuButton {
-                let gameData = gameDataFactory.createGame(fromLevel: level)
+                let gameData = gameManager.createGame(fromLevel: level)
                 slidingGame.segueWith(ChengYuSlidingViewModel(time: GameConstants.Sliding.time,
                                                               gameData: gameData,
                                                               rows: GameConstants.Sliding.rows))
             } else if (sender as? UIButton) === slidingCiHuiButton {
                 level.gameType = .ciHui
-                let gameData = gameDataFactory.createGame(fromLevel: level)
+                let gameData = gameManager.createGame(fromLevel: level)
                 slidingGame.segueWith(CiHuiSlidingViewModel(time: GameConstants.Sliding.time,
                                                             gameData: gameData,
                                                             rows: GameConstants.Sliding.rows))
