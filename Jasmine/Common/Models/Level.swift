bd1e7186eaa5748bed3e7d32d3e3ca17c37ecb12
Li Kai <falconets25601@gmail.com>
2017-04-08 12:25:33 +0800

    Add level object

---
+++ Jasmine/Common/Models/Level.swift
@@ -0,0 +1,50 @@
+import RealmSwift
+
+class Level: Object {
+
+    dynamic var name: String = "Untitled Level"
+    // difficulty of the level, higher means more difficult
+    // 1 since realm doesn't allow non-default values
+    dynamic var difficulty: Int = 1
+
+    private dynamic var rawGameType: String = GameType.ciHui.rawValue
+    private dynamic var rawGameMode: String = GameMode.swapping.rawValue
+
+    override static func primaryKey() -> String? {
+        return "name"
+    }
+
+    /// MARK: non-persisted properties
+    /// GameType of level, returns cihui by default
+    var gameType: GameType {
+        get {
+            guard let gameType = GameType(rawValue: rawGameType) else {
+                assertionFailure("Game type is invalid")
+                return GameType.ciHui
+            }
+            return gameType
+        }
+        set {
+            rawGameType = newValue.rawValue
+        }
+    }
+
+    /// GameMode of the level, returns swapping by default
+    var gameMode: GameMode {
+        get {
+            guard let gameMode = GameMode(rawValue: rawGameType) else {
+                assertionFailure("Game mode is invalid")
+                return GameMode.swapping
+            }
+            return gameMode
+        }
+        set {
+            rawGameMode = newValue.rawValue
+        }
+    }
+
+    override static func ignoredProperties() -> [String] {
+        return ["gameType", "gameMode"]
+    }
+
+}
