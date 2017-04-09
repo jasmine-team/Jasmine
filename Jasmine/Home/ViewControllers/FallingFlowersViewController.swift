9cc51ecdb9b516d37d769d5259b58075e4a7d5de
Wang Xien Dong <xdrawks@users.noreply.github.com>
2017-04-09 17:30:30 +0800

    Home Screen View Controller (#111)
    * Fix overflowing text in PhraseVC
    
    * Transiting to accelerometer based falling flowers
    
    * Add coloured flowers
    
    * completed initial implementation of falling flowers vc
    
    * Updated values in falling flowers
    
    * Add view elements into home view
    
    * add image assets
    
    * Update home screen with the icons
    
    * add image assets
    
    * Add coloured flowers
    
    * reconnect debug screen
    
    * Minor adjustments
    
    * Misc updates

---
+++ Jasmine/Home/ViewControllers/FallingFlowersViewController.swift
@@ -0,0 +1,99 @@
+import UIKit
+import CoreMotion
+
+/// A background of falling flowers.
+class FallingFlowersViewController: UIViewController {
+
+    // MARK: Constants
+    private static let spawnCount = 180
+
+    private static let flowerSizeMin = 20.0
+    private static let flowerSizeMax = 60.0
+
+    private static let accelerometerUpdateInterval = 0.05
+    private static let gravityMagnitude: CGFloat = 0.001
+
+    // MARK: Animation Properties
+    private var animator: UIDynamicAnimator!
+    private var gravityBehaviour: UIGravityBehavior!
+    private var collisionBehaviour: UICollisionBehavior!
+
+    private let accelerometer = CMMotionManager()
+
+    // MARK: Generator Properties
+    private var hasSpawned = false
+
+    private var flowerSize: CGSize {
+        let width = Random.double(from: FallingFlowersViewController.flowerSizeMin,
+                                  toInclusive: FallingFlowersViewController.flowerSizeMax)
+        return CGSize(width: width, height: width)
+    }
+
+    private var flowerSource: CGPoint {
+        let randomX = Random.double(from: Double(view.bounds.minX),
+                                    toInclusive: Double(view.bounds.maxX))
+        return CGPoint(x: randomX, y: 0)
+    }
+
+    // MARK: View Controller Lifecycle
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if !hasSpawned {
+            startAnimation()
+            for _ in 0..<FallingFlowersViewController.spawnCount {
+                generateFlower()
+            }
+            hasSpawned = true
+        }
+        startAccelerometerUpdate()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        accelerometer.stopAccelerometerUpdates()
+    }
+
+    /// Generates a flower and paste it in the current view.
+    private func generateFlower() {
+        let flower = UIImageView(image: #imageLiteral(resourceName: "flower-coloured"))
+        flower.frame = CGRect(center: flowerSource, size: flowerSize)
+        view.addSubview(flower)
+
+        gravityBehaviour.addItem(flower)
+        collisionBehaviour.addItem(flower)
+    }
+
+    // MARK: Animation Methods
+    private func startAnimation() {
+        specifyGravityBehaviour()
+        specifyCollisionBehaviour()
+
+        animator = UIDynamicAnimator(referenceView: view)
+        animator.addBehavior(gravityBehaviour)
+        animator.addBehavior(collisionBehaviour)
+    }
+
+    private func startAccelerometerUpdate() {
+        accelerometer.accelerometerUpdateInterval
+            = FallingFlowersViewController.accelerometerUpdateInterval
+
+        accelerometer.startAccelerometerUpdates(to: .main) { data, _ in
+            guard let data = data else {
+                return
+            }
+            let direction = CGVector(dx: data.acceleration.x, dy: -data.acceleration.y)
+            self.gravityBehaviour.gravityDirection = direction
+        }
+    }
+
+    private func specifyGravityBehaviour() {
+        gravityBehaviour = UIGravityBehavior()
+        gravityBehaviour.magnitude = FallingFlowersViewController.gravityMagnitude
+    }
+
+    private func specifyCollisionBehaviour() {
+        collisionBehaviour = UICollisionBehavior()
+        collisionBehaviour.translatesReferenceBoundsIntoBoundary = true
+    }
+
+}
