e5a6270eb00d57a5c528b2d9bd60d0982a1d1f27
Li Kai <falconets25601@gmail.com>
2017-04-09 13:14:58 +0800

    Add default phrases to realm

--- JasmineTests/Common/Support/RealmTestCase.swift
+++ JasmineTests/Common/Support/RealmTestCase.swift
@@ -6,6 +6,17 @@ class RealmTestCase: XCTestCase {
 
     var realm: Realm!
     var gameDataFactory: GameManager!
+    
+    static let defaultPhrases = [
+        Phrase(value: ["rawChinese": "什么", "rawPinYin": "shen me", "english": "test"]),
+        Phrase(value: ["rawChinese": "没有", "rawPinYin": "mei you", "english": "test"]),
+        Phrase(value: ["rawChinese": "自己", "rawPinYin": "zi ji", "english": "test"]),
+        Phrase(value: ["rawChinese": "知道", "rawPinYin": "zhi dao", "english": "test"]),
+        Phrase(value: ["rawChinese": "谈何容易", "rawPinYin": "tán hé róng yì", "english": "test"]),
+        Phrase(value: ["rawChinese": "一窍不通", "rawPinYin": "yī qiào bù tōng", "english": "test"]),
+        Phrase(value: ["rawChinese": "一鸣惊人", "rawPinYin": "yī míng jīng rén", "english": "test"]),
+        Phrase(value: ["rawChinese": "不可思议", "rawPinYin": "bù kě sī yì", "english": "test"]),
+    ]
 
     override func setUp() {
         super.setUp()
@@ -29,7 +40,8 @@ class RealmTestCase: XCTestCase {
         }
     }
 
-    func createGameData(phrases: [Phrase], difficulty: Int, type: GameType) -> GameData {
+    func createGameData(phrases: [Phrase] = RealmTestCase.defaultPhrases, 
+                        difficulty: Int, type: GameType) -> GameData {
         phrases.forEach(save)
         let level = Level(value: [
             "rawGameType": type.rawValue,
