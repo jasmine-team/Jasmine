1351ed3ac274b606704b0bad84d4720382fddb7c
Herbert Ilhan Tanujaya <h.i@u.nus.edu>
2017-04-10 17:14:57 +0800

    change reload level to accept bool

--- Jasmine/Common/ViewControllers/LevelSelectorViewControllerDelegate.swift
+++ Jasmine/Common/ViewControllers/LevelSelectorViewControllerDelegate.swift
@@ -1,14 +1,10 @@
 /// The ViewController delegate for the LevelSelector. ViewModel will own this.
 protocol LevelSelectorViewControllerDelegate: class {
-    /// Reload the default level at the given index.
+    /// Reload the level at the given index.
     ///
     /// - Parameter index: the index for the level to be reloaded
-    func reloadDefaultLevel(at index: Int)
-
-    /// Reload the custom level at the given index.
-    ///
-    /// - Parameter index: the index for the level to be reloaded
-    func reloadCustomLevel(at index: Int)
+    /// - Parameter isDefault: whether the level is default or not
+    func reloadLevel(at index: Int, isDefault: Bool)
 
     /// Reload all levels.
     func reloadAllLevels()
