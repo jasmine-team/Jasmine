30694d537620671d292c702267727e0d23d98033
Herbert Ilhan Tanujaya <h.i@u.nus.edu>
2017-04-10 16:01:02 +0800

    update

--- Jasmine/Common/Models/GameInfo.swift
+++ Jasmine/Common/Models/GameInfo.swift
@@ -1,4 +1,5 @@
 struct GameInfo {
+    let uuid: String
     let levelName: String
     let gameType: GameType
     let gameMode: GameMode
--- Jasmine/Common/ViewModels/LevelSelectViewModelProtocol.swift
+++ Jasmine/Common/ViewModels/LevelSelectViewModelProtocol.swift
@@ -5,23 +5,14 @@ protocol LevelSelectorViewModelProtocol {
     /// The custom levels in the game.
     var customLevels: [GameInfo] { get }
 
-    /// Get all the phrases from the given level ID.
+    /// Get all the phrases from the given level UUID.
     ///
-    /// - Parameter levelID: the level ID
-    /// - Returns: all phrases in the level ID
-    func getLevelPhrases(from levelID: Int) -> [String]
-
-    /// Updates the custom level
-    ///
-    /// - Parameters:
-    ///   - levelID: the ID for the level
-    ///   - newName: the new name for the level
-    ///   - newGameMode: the game mode for the level
-    ///   - newGameType: the game type for the level
-    func updateLevelData(from levelID: Int, newName: String, newGameMode: GameMode, newGameType: GameType)
+    /// - Parameter levelID: the level UUID
+    /// - Returns: all phrases in the level UUID
+    func getLevelPhrases(from uuid: String) -> [String]
 
     /// Deletes the custom level
     ///
     /// - Parameter levelID: the level ID
-    func deleteLevel(from levelID: Int)
+    func deleteLevel(from uuid: String)
 }
