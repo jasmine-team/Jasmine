d5b770d92c0c2f89e287df7cba6fef5a4bfd3b52
riwu <riwu0730@gmail.com>
2017-04-11 19:11:07 +0800

    Implemented Levels.noPhraseSelected error and refactoring

--- Jasmine/Common/Helpers/LevelsError.swift
+++ Jasmine/Common/Helpers/LevelsError.swift
@@ -3,11 +3,14 @@ import Foundation
 enum LevelsError: LocalizedError {
 
     case duplicateLevelName(String)
+    case noPhraseSelected
 
     var errorDescription: String? {
         switch self {
         case .duplicateLevelName(let name):
             return "Duplicate level name: " + name
+        case .noPhraseSelected:
+            return "No Phrase selected"
         }
     }
 }
