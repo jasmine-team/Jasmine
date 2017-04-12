a7e85b1b487cdc7fa5be3f0aff458490d4d43e61
Herbert Ilhan Tanujaya <7744han@gmail.com>
2017-04-12 16:31:05 +0800

    added a simple About Us View + VC (#143)

---
+++ Jasmine/Home/ViewControllers/AboutViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class AboutViewController: UIViewController {
+    /// Dismisses this current screen when "Back" button is pressed.
+    @IBAction func onBackPressed(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
