f34a7ad81f441e5b48b821623e35f83141580caa
riwu <riwu0730@gmail.com>
2017-04-13 21:07:14 +0800

    Updated level designer and phrases explorer VCs Refactored alert functions into extension Moved import button from phrase explorer to level designer (yet to implement)
    Phrase explorer:
    Added save button (hidden if onSaveCallBack not set — i.e. not markable)
    If selected phrases are modified, back button will ask for conformation to exit without saving
    Removed isMarkable variable: assumed to be not markable if onSaveCallback is not set
    
    Level designer:
    Added onSaveCallBack so that level selector interface can be updated after saving
    
    Level selector:
    Added reloadAllLevels as argument to level designer onSaveCallBack

--- Jasmine/Common/Extensions/UIVIewController+Alert.swift
+++ Jasmine/Common/Extensions/UIVIewController+Alert.swift
@@ -5,13 +5,16 @@ extension UIViewController {
     private static let cancelActionText = "Cancel"
     private static let errorControllerTitle = "Error"
 
+    private static let exitWithoutSavingAlertTitle = "Exit without saving?"
+    private static let conformExitWithoutSavingActionText = "Yes"
+
     /// Returns an alert controller with cancel button
     ///
     /// - Parameters:
-    ///   - title: title of the alert
-    ///   - message: message for the alert
+    ///   - title: Title of the alert
+    ///   - message: Message for the alert
     /// - Returns: UIAlertController with cancel button
-    func getAlertControllerWithCancel(title: String, message: String? = nil) -> UIAlertController {
+    private func getAlertControllerWithCancel(title: String, message: String? = nil) -> UIAlertController {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: UIViewController.cancelActionText, style: .cancel))
         return alertController
@@ -26,11 +29,37 @@ extension UIViewController {
         present(getAlertControllerWithCancel(title: title, message: message), animated: true)
     }
 
+    /// Display an alert pop up with a cancel button and another button with `actionText` 
+    /// and executes `onActionButtonPressed` on pressed
+    ///
+    /// - Parameters:
+    ///   - title: Title of the alert
+    ///   - actionText: Message for the alert
+    ///   - onActionButtonPressed: Callback to execute when button is pressed
+    func showAlert(title: String, actionText: String, onActionButtonPressed: @escaping () -> Void) {
+        let alert = getAlertControllerWithCancel(title: title)
+        let action = UIAlertAction(title: actionText, style: .destructive) { _ in
+            onActionButtonPressed()
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
+
+    }
+
     /// Displays an error pop up with a cancel button
     ///
     /// - Parameters:
     ///   - error: the error to show
     func showError(_ error: Error) {
         showAlert(title: UIViewController.errorControllerTitle, message: error.localizedDescription)
+    }
+
+    /// Displays an exit without saving alert, which dismisses the view if conformed to exit.
+    /// Returns to the current view if cancel is selected
+    func showExitWithoutSavingAlert() {
+        showAlert(title: UIViewController.exitWithoutSavingAlertTitle,
+                  actionText: UIViewController.conformExitWithoutSavingActionText) {
+            self.dismiss(animated: true)
+        }
     }
 }
