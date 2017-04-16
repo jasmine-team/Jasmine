17c9d2d37c6ee9bb0bfc8996dfd01f499f1aedc3
Li Kai <falconets25601@gmail.com>
2017-04-16 15:38:01 +0800

    Update scripts for game center related topics

--- Jasmine/Common/Helpers/GameCenterNotifier.swift
+++ Jasmine/Common/Helpers/GameCenterNotifier.swift
@@ -4,7 +4,11 @@ import RealmSwift
 
 /// Helper functions to report to game center
 class GameCenterNotifier {
-    
+
+    static let bronzePlaycount = (requiredCount: 10, identifier: "combined.playcount.bronze")
+    static let silverPlaycount = (requiredCount: 100, identifier: "combined.playcount.silver")
+    static let goldPlaycount = (requiredCount: 1000, identifier: "combined.playcount.gold")
+
     fileprivate static let UUIDToIdentifier: [String: String] = {
         let uuids = [
             "EE8A0045-4B64-4840-855D-584C283FBE1F"
@@ -17,32 +21,37 @@ class GameCenterNotifier {
     }()
     
     let realm: Realm
-    let results: Results<LevelResult>
-    let player: Player
+    let results: ResultsQueryContainer
+
     private var resultsNotifier: NotificationToken?
+
+    var gameCenterReporter: (Error?) -> Void {
+        return { error in
+            guard let error = error else {
+                return
+            }
+            print(error.localizedDescription)
+        }
+    }
     
-    init(realm: Realm, player: Player) {
+    init(realm: Realm) {
         self.realm = realm
-        self.player = realm.objects(Player.self).first ?? Player()
-        results = realm.objects(LevelResult.self)
+        results = ResultsQueryContainer(realm: realm)
         attachListeners()
     }
     
     func attachListeners() {
-        resultsNotifier = results.addNotificationBlock(resultsChanged)
+        resultsNotifier = results.levelResults.addNotificationBlock(resultsChanged)
     }
     
     func resultsChanged(changes: RealmCollectionChange<Results<LevelResult>>) {
-        print("TEST")
         switch changes {
         case .initial:
-            print("IS WORKING")
             return
         case .update(_, _, let insertions, _):
             // results will only ever be inserted, not updated or deleted (unless user reset)
-            print("UPDATE")
             insertions.forEach { tester in
-                updateFor(result: results[tester])
+                updateFor(result: results.levelResults[tester])
             }
         case .error(let error):
             // An error occurred while opening the Realm file on the background worker thread
@@ -55,42 +64,59 @@ class GameCenterNotifier {
             assertionFailure("Level was not attached")
             return
         }
+        updateTotalPlaycount()
         guard let identifier = GameCenterNotifier.UUIDToIdentifier[level.uuid] else {
             return  // not a original level
         }
         submit(score: result.score, for: identifier)
     }
     
-    func submit(score: Int, for identifier: String) {
-        print("SUBMITTING")
+    /// Updates total playcount achievement
+    private func updateTotalPlaycount() {
+        let playcount = results.levelResults.count
+        
+        let bronze = GameCenterNotifier.bronzePlaycount
+        let silver = GameCenterNotifier.silverPlaycount
+        let gold = GameCenterNotifier.goldPlaycount
+        
+        switch playcount {
+        case 0...bronze.requiredCount:
+            submit(achievementProgress: Double(playcount / bronze.requiredCount), 
+                   for: bronze.identifier)
+        case bronze.requiredCount...silver.requiredCount:
+            submit(achievementProgress: Double(playcount / silver.requiredCount), 
+                   for: silver.identifier)
+        case silver.requiredCount...gold.requiredCount:
+            submit(achievementProgress: Double(playcount / gold.requiredCount), 
+                   for: gold.identifier)
+        default:
+            return  // maxed out achievement
+        }
+    }
+    
+    /// Helper function to submite game center score
+    ///
+    /// - Parameters:
+    ///   - score: score to report
+    ///   - identifier: identifier of the leaderboards
+    private func submit(score: Int, for identifier: String) {
         // Submit score to GC leaderboard
         let bestScoreInt = GKScore(leaderboardIdentifier: identifier)
         bestScoreInt.value = Int64(score)
-        GKScore.report([bestScoreInt]) { error in
-            guard let error = error else {
-                return
-            }
-            print(error.localizedDescription)
-        }
+        GKScore.report([bestScoreInt], withCompletionHandler: gameCenterReporter)
     }
-    
-    func achieve() {
-        let achievement = GKAchievement(identifier: "test.first")
-        
-        achievement.percentComplete = 100
+
+    /// Submits achievement progress to game center
+    ///
+    /// - Parameters:
+    ///   - achievementProgress: progress in terms of percentage
+    ///   - identifier: identifier of achievement
+    private func submit(achievementProgress: Double, for identifier: String) {
+        let achievement = GKAchievement(identifier: identifier)
+        achievement.percentComplete = achievementProgress
         achievement.showsCompletionBanner = true  // use Game Center's UI
         
-        GKAchievement.report([achievement]) { error in
-            guard let error = error else {
-                return
-            }
-            print(error.localizedDescription)
-        }
-    }
-    
-    // Detach listeners to realm collections
-    deinit {
-        resultsNotifier = nil
+        GKAchievement.report([achievement], withCompletionHandler: gameCenterReporter)
     }
     
 }
--- Jasmine/Common/Models/ResultsQueryContainer.swift
+++ Jasmine/Common/Models/ResultsQueryContainer.swift
@@ -1,9 +1,13 @@
 import RealmSwift
 
-class ProfileQueryContainer {
+class ResultsQueryContainer {
 
     let realm: Realm
     let levelResults: Results<LevelResult>
+    
+    let slidingResults: Results<LevelResult>
+    let tetrisResults: Results<LevelResult>
+    let swappingResults: Results<LevelResult>
 
     /// Initialize manager with realm instance, useful for testing
     ///
@@ -11,15 +15,19 @@ class ProfileQueryContainer {
     init(realm: Realm) {
         self.realm = realm
         levelResults = realm.objects(LevelResult.self)
+        
+        slidingResults = levelResults.filter("ANY level.gameMode = 'sliding'")
+        tetrisResults = levelResults.filter("ANY level.gameMode = 'tetris'")
+        swappingResults = levelResults.filter("ANY level.gameMode = 'swapping'")
     }
-
+    
     /// Initiates the query container with a default realm
     ///
     /// - Throws: realm error if unable to initialize
     convenience init() throws {
         self.init(realm: try Realm())
     }
-
+    
     /// Returns times played between the two days
     ///
     /// - Parameters:
@@ -31,5 +39,5 @@ class ProfileQueryContainer {
                                     argumentArray: [startDate, endDate])
         return levelResults.filter(predicate).count
     }
-
+    
 }
